70dfdc3a02faab2b3bfcc7c131ba47ff4e24a4e6
Joao Sironi <joaovsironi@gmail.com>
2022-05-22 22:01:10 -0300

    feat(Trainer): create model of treiners

---
+++ src/api/Pokemon.TheBestTeam.Api/Models/Trainer.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon.TheBestTeam.Api.Models;
+{
+    public record Trainer(
+        string name,
+        string password,
+        string email,
+        string nickname,
+        string image
+    );
+}
