4a27d1cab141e4111ec778c5ce087e8cbac06849
Joao Sironi <joaovsironi@gmail.com>
2022-05-22 21:59:02 -0300

    feat(Trainer): Creating endpoints to CRUD of trainer pokemons
    Begins create trainer controller to CRUD of trainer pokemons

---
+++ src/api/Pokemon.TheBestTeam.Api/Controllers/TrainerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokemon.TheBestTeam.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TrainerControler : ControllerBase
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok("Will be implemented");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetTrainer(string uuid)
+    {
+        return Ok("Will be implemented");
+    }
+
+    [HttpPost]
+    public IActionResult CreateTrainer(Models.NewDrinkModel drink)
+    {
+        return Ok("Will be implemented");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateTrainer(string uuid, object drink)
+    {
+        return Ok("Will be implemented");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteTrainer(string uuid)
+    {
+        return Ok("Will be implemented");
+    }
+}
